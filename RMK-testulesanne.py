b2783aa04d46270d2ffea91ca21de4ed0d6856fe
Kenri-Benrot <kenri.benrot@gmail.com>
2025-05-30 17:48:36 +0300

    Simulatsiooni lisamine ning koodinimetuste parandamine.

--- RMK-testulesanne.py
+++ RMK-testulesanne.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
 
 # Aeg, mis kulub Rital liigeldes
 kodust_bussipeatus = timedelta(seconds=300)
@@ -9,7 +10,7 @@ bussipeatusest_toole = timedelta(seconds=240)
 koosolek = datetime.strptime("09:05", "%H:%M")
 
 #Bussi graafiku loomine, et saaks täpsemalt arvutada, millal bussid liiguvad.
-def loo_bussigraafik(algus="08:05", lopp="08:59", interval_minutites=10):
+def bussigraafik(algus="08:05", lopp="08:59", interval_minutites=10):
     graafik = []
     algusaeg = datetime.strptime(algus, "%H:%M")
     lopp_aeg = datetime.strptime(lopp, "%H:%M")
@@ -21,27 +22,48 @@ def loo_bussigraafik(algus="08:05", lopp="08:59", interval_minutites=10):
     return graafik
 
 # Kontrollib kas Rita jõuab koosolekule õigeks ajaks või mitte.
-def saa_saabumise_staatus(lahkumise_aeg_str, bussigraafik):
-    lahkumise_aeg = datetime.strptime(lahkumise_aeg_str, "%H:%M")
+def saabumine(lahkumise_aeg, bussigraafik):
+    lahkumise_aeg = datetime.strptime(lahkumise_aeg, "%H:%M")
 
     # Millal Rita jõuab bussipeatusesse
     bussipeatuses = lahkumise_aeg + kodust_bussipeatus
 
     # Otsib bussigraafikust bussi, mis läheb, kui Rita jõuab bussipeatusesse
     sobiv_buss = next((buss for buss in bussigraafik if buss >= bussipeatuses), None)
-    if sobiv_buss is None:
-        print("ei jõua tööle!")
-        return False
+    if not sobiv_buss:
+        return True
     
     # Aruvtamine, millal Rita jõuab tööle
     saabumine_toole = sobiv_buss + bussisoit + bussipeatusest_toole
-    
-    # Esitab vastuse
-    if saabumine_toole <= koosolek:
-        print("Jõuad kohale!")
-    else:
-        print("Ei jõua!")
+    return saabumine_toole > koosolek
 
-bussigraafik = loo_bussigraafik()
-lahkumise_aeg = "08:40"
-tulemus = saa_saabumise_staatus(lahkumise_aeg, bussigraafik)
+def simulatsioon():
+    bussid = bussigraafik ()
+    ajad = []
+    hilinemised = []
+
+    algus = datetime.strptime("08:00", "%H:%M")
+    lopp = datetime.strptime("09:00", "%H:%M")
+    hetkeaeg = algus
+
+    while hetkeaeg <= lopp:
+        aeg = hetkeaeg.strftime("%H:%M")
+        ajad.append(hetkeaeg)
+        hilineb = saabumine(aeg, bussid)
+        hilinemised.append(1 if hilineb else 0)
+        hetkeaeg += timedelta(minutes=1)
+
+    # Graafiku joonistamine
+    plt.style.use("dark_background")
+    plt.figure(figsize=(8, 5))
+    plt.plot(ajad, hilinemised, color="red", linewidth=4)
+    plt.xlabel("Millal Rita kodust lahkub")
+    plt.ylabel("Jääb hiljaks")
+    plt.title("Tõenäosus kas Rita hilineb koosolekule.")
+    plt.ylim(-0.05, 1.05)
+    plt.grid(True, linestyle="--", alpha=0.2)
+    plt.tight_layout()
+    plt.show()
+
+# Käivitamine
+simulatsioon()
